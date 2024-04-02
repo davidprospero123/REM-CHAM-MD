450641e2f916d4a797897918c9037858514de3da
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-04-02 02:39:58 -0500

    REM-BOT

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
-╭────═[ *𝘾𝙍𝙀𝘼𝘿𝙊𝙍* ]═─────⋆
+╭────═[ *_𝘾𝙍𝙀𝘼𝘿𝙊𝙍_* ]═─────⋆
 │╭───────────────···
 ┴│✯ *🗳️ ɴᴏᴍʙʀᴇ* : ᴄᴜʀɪ
 ✩│✯  𝙉𝙧𝙤 : 
@@ -10,14 +10,11 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 ┬ʀᴇᴄᴜᴇʀᴅᴀ ɴᴏ ᴅᴇʙᴇꜱ
 │ᴇꜱᴄʀɪʙɪʀʟᴇ ᴀ ᴍɪ ᴄʀᴇᴀᴅᴏʀ ᴀ
 │ᴍᴇɴᴏꜱ Qᴜᴇ ꜱᴇᴀ ᴅᴇ ᴍᴜᴄʜᴀ
-│ᴜʀɢᴇɴᴄɪᴀ ꜱɪ Qᴜɪᴇʀᴇ ʜᴀʙʟᴀʀ ᴇꜱ
-│ᴄᴏɴ ᴍɪ ᴍᴏᴅᴇʀᴀᴅᴏʀᴀ ᴄᴏɴ ᴇʟ
-│ᴄᴏᴍᴀɴᴅᴏ 
-│✯ .𝘮𝘰𝘥𝘦𝘳𝘢𝘥𝘰𝘳𝘢 , .𝘮𝘰𝘥
+│ᴜʀɢᴇɴᴄɪᴀ.
 ╰───────────═┅═──────
 
 `
-let img = 'https://telegra.ph/file/ea6467f35579c3eaf0a09.png'
+let img = 'https://telegra.ph/file/c775e80b9043a37191b98.png'
 conn.sendFile(m.chat, img, 'img.png', don, m, null, rpyp)
 //conn.sendPayment(m.chat, '2000', 'USD', don, m.sender, m)
 }
