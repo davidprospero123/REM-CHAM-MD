a586911ed1e3c0ba2e53fc35bbfa8a44f20870a3
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-04-05 01:32:47 -0500

    REM-BOT

--- plugins/dl-yts.js
+++ plugins/dl-yts.js
@@ -1,37 +1,37 @@
-import axios from 'axios';
+import yts from "yt-search";
 
-let handler = async (m, { conn, text }) => {
-  if (!text) throw '✳️ ¿Qué quieres que busque en YouTube?';
-
-  try {
-    const query = encodeURIComponent(text);
-    const response = await axios.get(`https://weeb-api.vercel.app/ytsearch?query=${query}`);
-    const results = response.data;
-
-    if (results.length === 0) {
-      throw 'No hay ningun Resultado :c.';
-    }
-
-    const firstResult = results[0];
-
-    const message = `
-乂 ${firstResult.title}
-乂 *Link* : ${firstResult.url}
-乂 *Duracion* : ${firstResult.timestamp}
-乂 *Publicado :* ${firstResult.ago}
-乂 *Visitas:* ${firstResult.views}
-
-    `;
-
-    conn.sendFile(m.chat, firstResult.thumbnail, 'yts.jpeg', message, m);
-  } catch (error) {
-    console.error(error);
-    throw 'Hay un error con el buscador de videos de YouTube.';
+let handler = async (m, { conn, usedPrefix, text }) => {
+  if (!text)
+    return conn.reply(
+      m.chat,
+      "*🚩 Ingresa lo que deseas buscar en YouTube.*",
+      m,
+    );
+  await m.react("💙");
+  let results = await yts(text);
+  let res = results.all.map((v) => v).filter((v) => v.type == "video");
+  if (!res.length)
+    return conn
+      .reply(
+        m.chat,
+        "No se encontraron resultados, intente con un nombre más Corto.",
+        m,
+      )
+      .then((_) => m.react("✖️"));
+  let txt = `*ＹｏｕＴｕｂｅ-Ｓｅａｒｃｈ ⇄ Ⅰ<    ⅠⅠ    >Ⅰ   ↻*`;
+  for (let i = 0; i < (30 <= res.length ? 30 : res.length); i++) {
+    txt += `\n\n`;
+    txt += `	❧  *ᴛɪᴛᴜʟᴏ* : ${res[i].title}\n`;
+    txt += `	❧  *ᴅᴜʀᴀᴄɪÓɴ* : ${res[i].timestamp || "×"}\n`;
+    txt += `	❧  *ᴘᴜʙʟɪᴄᴀᴅᴏ* : ${res[i].ago}\n`;
+    txt += `	❧  *ᴀᴜᴛᴏʀ* : ${res[i].author.name || "×"}\n`;
+    txt += `	❧  *ᴜʀʟ* : ${"https://youtu.be/" + res[i].videoId}\n`;
   }
+  await conn.sendFile(m.chat, res[0].image, "", txt, m);
+  await m.react("✅");
 };
-
-handler.help = ['ytsearch'];
-handler.tags = ['downloader'];
-handler.command = ['ytsearch', 'yts'];
-
+handler.help = ["ytsearch"];
+handler.tags = ["search"];
+handler.command = ["ytsearch", "yts"];
+handler.register = true;
 export default handler;
