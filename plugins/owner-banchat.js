8225c48da07bd732c8e189ebcf5acb5585db1323
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-04-12 18:53:41 -0500

    REM-BOT

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,12 +1,13 @@
 //import db from '../lib/database.js'
 
-let handler = async (m, { conn, isOwner, isAdmin, isROwner} ) => {
+let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = false
-    m.reply('✅ Bot activado en este grupo')   
+    global.db.data.chats[m.chat].isBanned = true
+    m.reply('✅ 𝙴𝙻 𝙱𝙾𝚃 𝙵𝚄𝙴 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾')
 }
-handler.help = ['unbanchat']
+handler.help = ['banchat']
 handler.tags = ['owner']
-handler.command = ['chaton', 'unbanchat'] 
+handler.command = ['banchat', 'chatoff'] 
 
 export default handler
+ 
