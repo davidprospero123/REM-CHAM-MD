1682fa97895d359bdb1ef92c6d0898a62878dd9d
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-04-15 16:30:07 -0500

    REM-BOT

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -1,22 +1,18 @@
 import { canLevelUp, xpRange } from '../lib/levelling.js';
 import axios from 'axios';
 
-
 const challenges = [
     { name: '𝙴𝚗𝚟𝚒𝚊𝚛 𝟷𝟶𝟶 𝚖𝚎𝚗𝚜𝚊𝚓𝚎𝚜', xpReward: 50 },
     { name: '𝙿𝚊𝚛𝚝𝚒𝚌𝚒𝚙𝚊𝚛 𝚎𝚗 𝟻 𝚎𝚟𝚎𝚗𝚝𝚘𝚜 𝚍𝚎𝚕 𝚐𝚛𝚞𝚙𝚘', xpReward: 100 },
     { name: '𝙸𝚗𝚟𝚒𝚝𝚊𝚛 𝚊 𝟹 𝚗𝚞𝚎𝚟𝚘𝚜 𝚖𝚒𝚎𝚖𝚋𝚛𝚘𝚜 700xᴘ', xpReward: 700 }
 ];
 
-
 const completedChallenges = {};
-
 
 function hasCompletedChallenge(userId, challengeName) {
     return completedChallenges[userId]?.includes(challengeName);
 }
 
-a
 function completeChallenge(userId, challengeName) {
     completedChallenges[userId] = completedChallenges[userId] || [];
     completedChallenges[userId].push(challengeName);
@@ -28,7 +24,7 @@ let handler = async (m, { conn }) => {
 
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier);
-        let progress = Math.floor(((user.exp - min) / xp) * 10);
+        let progress = Math.min(Math.max(Math.floor(((user.exp - min) / xp) * 10), 0), 10); 
         let progressBar = "▰".repeat(progress) + "▱".repeat(10 - progress);
         let lvl = `
 ╭───[ 𝚂𝚄𝙱𝙴𝚂 𝙳𝙴 𝙽𝙸𝚅𝙴𝙻 ]───
@@ -63,7 +59,7 @@ let handler = async (m, { conn }) => {
 ╭────[ 𝚂𝚄𝙱𝙴𝚂 𝙳𝙴 𝙽𝙸𝚅𝙴𝙻 ]────
 │ 🌟 𝙽𝙾𝙼𝙱𝚁𝙴: ${name}
 │ 🎖️ 𝙽𝙸𝚅𝙴𝙻 𝙰𝙲𝚃𝚄𝙰𝙻: ${user.level}
-│ 🎖️ 𝙽𝙸𝚅𝙴𝙻 𝙰𝙲𝚃𝚄𝙰𝙻: ${before}
+│ 🎖️ 𝙽𝙸𝚅𝙴𝙻 𝙰𝙽𝚃𝙴𝚁𝙸𝙾𝚁: ${before}
 │
 │ ¡𝙵𝚎𝚕𝚒𝚌𝚒𝚍𝚊𝚍𝚎𝚜, ${name}! 𝙷𝚊𝚜 𝚜𝚞𝚋𝚒𝚍𝚘 𝚍𝚎 𝚗𝚒𝚟𝚎𝚕.
 ╰────────═┅═──────────
