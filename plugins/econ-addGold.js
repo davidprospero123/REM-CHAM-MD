d5014b617cd2936bac160eb2a16deca6e920fcab
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-04-15 16:56:34 -0500

    REM-BOT

--- plugins/econ-addGold.js
+++ plugins/econ-addGold.js
@@ -1,29 +1,50 @@
-let handler = async (m, { conn, text }) => {
-  let who
-  if (m.isGroup) who = m.mentionedJid[0]
-  else who = m.chat
-  if (!who) throw '✳️ Etiquetar al usuario'
-  let txt = text.replace('@' + who.split`@`[0], '').trim()
-  if (!txt) throw '✳️ Ingresa la cantidad de *Oro* que deseas agregar'
-  if (isNaN(txt)) throw '🔢 sólo números'
-  let dmt = parseInt(txt)
-  let diamond = dmt
+import axios from 'axios';
 
-  if (diamond < 1) throw '✳️ Minimo *1*'
-  let users = global.db.data.users
- users[who].credit += dmt
+let handler = async (m, { conn, usedPrefix, args }) => {
+    let user = global.db.data.users[m.sender];
+    let mentionedUser = m.mentionedJid[0];
+    let goldAmount = args[1] ? parseInt(args[1]) : 0;
 
-  await m.reply(`≡ *Gold Agregado*
-┌──────────────
-▢ *Total:* ${dmt}
-└──────────────`)
- conn.fakeReply(m.chat, `▢ Recibió usted \n\n *+${dmt}* Gold`, who, m.text)
+    if (!mentionedUser) 
+        throw '𝙿𝚘𝚛 𝚏𝚊𝚟𝚘𝚛, 𝚖𝚎𝚗𝚌𝚒𝚘𝚗𝚊 𝚊𝚕 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚊𝚕 𝚚𝚞𝚎 𝚍𝚎𝚜𝚎𝚊𝚜 𝚍𝚊𝚛 𝚘𝚛𝚘.';
+    
+    if (!args[1] || isNaN(goldAmount) || goldAmount < 1) 
+        throw '𝙿𝚘𝚛 𝚏𝚊𝚟𝚘𝚛, 𝚒𝚗𝚝𝚛𝚘𝚍𝚞𝚌𝚎 𝚞𝚗𝚊 𝚌𝚊𝚗𝚝𝚒𝚍𝚊𝚍 𝚟á𝚕𝚒𝚍𝚊 𝚍𝚎 𝚘𝚛𝚘 𝚙𝚊𝚛𝚊 𝚍𝚊𝚛. 𝙴𝚓𝚎𝚖𝚙𝚕𝚘: .𝚍𝚊𝚛𝚘𝚛𝚘 @𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝟷𝟶𝟶';
+
+    if (user.credit < goldAmount) 
+        throw '𝙽𝚘 𝚝𝚒𝚎𝚗𝚎𝚜 𝚜𝚞𝚏𝚒𝚌𝚒𝚎𝚗𝚝𝚎 𝚘𝚛𝚘 𝚙𝚊𝚛𝚊 𝚛𝚎𝚊𝚕𝚒𝚣𝚊𝚛 𝚎𝚜𝚝𝚊 𝚝𝚛𝚊𝚗𝚜𝚏𝚎𝚛𝚎𝚗𝚌𝚒𝚊.';
+
+    let recipient = global.db.data.users[mentionedUser];
+    recipient.credit += goldAmount;
+    user.credit -= goldAmount;
+
+    let message = `
+🎁 *𝚃𝚛𝚊𝚗𝚜𝚏𝚎𝚛𝚎𝚗𝚌𝚒𝚊 𝚍𝚎 𝙾𝚛𝚘 𝚁𝚎𝚊𝚕𝚒𝚣𝚊𝚍𝚊.* 🎁
+
+💰 *Cantidad Transferida*: ${goldAmount} oro
+👤 *Destinatario*: @${mentionedUser.split('@')[0]}
+👥 *Tu Saldo Actual*: ${user.credit} oro
+
+¡𝙻𝚊 𝚝𝚛𝚊𝚗𝚜𝚏𝚎𝚛𝚎𝚗𝚌𝚒𝚊 𝚍𝚎 𝚘𝚛𝚘 𝚜𝚎 𝚑𝚊 𝚌𝚘𝚖𝚙𝚕𝚎𝚝𝚊𝚍𝚘 𝚌𝚘𝚗 é𝚡𝚒𝚝𝚘! 🌟✨
+
+_🔹 ¡𝙳𝚒𝚗𝚎𝚛𝚘, 𝚍𝚒𝚗𝚎𝚛𝚘, 𝚍𝚒𝚗𝚎𝚛𝚘! 💰💸🔹_
+`.trim();
+
+    try {
+        const imgUrl = 'https://i.imgur.com/Em0VBOJ.png';
+        const responseImg = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+        await conn.sendFile(m.chat, responseImg.data, "thumbnail.jpg", message, m);
+    } catch (e) {
+        await conn.reply(m.chat, message, m);
+    }
 }
 
-handler.help = ['addgold <@user>']
-handler.tags = ['economy']
-handler.command = ['addgold','depositar','depositgold','deposit'] 
-handler.rowner = true
+handler.help = ['daroro'];
+handler.tags = ['economy'];
+handler.command = ['daroro', 'transferiroro', 'dor'];
 
-export default handler
+export default handler;
 
+// Ejemplo de uso
+// Comando: .daroro @usuario 100
+// Texto: Transfiere 100 oro al usuario mencionado.
