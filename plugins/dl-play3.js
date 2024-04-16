aae3c92e2233d4e45c2f4b9d2a66e6e1a127aff1
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-04-16 02:13:57 -0500

    REM-BOT

--- plugins/dl-play3.js
+++ plugins/dl-play3.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 
 
-const imgUrl = 'https://i.imgur.com/APg4Nl7.jpg';
+const imgUrl = 'https://i.imgur.com/JdMcnNQ.jpeg';
 
 let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     let lister = ["mp3", "yta", "audio", "ytv", "video", "vídeo", "mp4", "mp3doc", "ytadoc", "audiodoc", "mp4doc", "ytvdoc", "videodoc", "vídeodoc"];
