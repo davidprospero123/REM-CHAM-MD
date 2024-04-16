fdf1f8d0977c6dc3f6b769aa0f825147e0eadc0f
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-04-15 23:00:36 -0500

    REM-BOT

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -1,19 +1,35 @@
-//import db from '../lib/database.js'
+import { createHash } from 'crypto';
+import axios from 'axios';
 
-import { createHash } from 'crypto'
-let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `✳️ *Ingrese número de serie*\nVerifique su número de serie con el comando...\n\n*${usedPrefix}nserie*`
-  let user = global.db.data.users[m.sender]
-  let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw '⚠️ *Número de serie incorrecto*'
-  user.registered = false
-  m.reply(`✅ Registro eliminado`)
+let handler = async function (m, { conn, args, usedPrefix }) {
+    if (!args[0]) throw `✳️ 𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚝𝚞 𝚗ú𝚖𝚎𝚛𝚘 𝚍𝚎 𝚜𝚎𝚛𝚒𝚎\n𝚅𝚎𝚛𝚒𝚏𝚒𝚌𝚊 𝚝𝚞 𝚗ú𝚖𝚎𝚛𝚘 𝚍𝚎 𝚜𝚎𝚛𝚒𝚎 𝚌𝚘𝚗 𝚎𝚕 𝚌𝚘𝚖𝚊𝚗𝚍𝚘...\n\n*${usedPrefix}nserie*`;
+
+    let sn = createHash('md5').update(m.sender).digest('hex');
+
+    if (args[0] !== sn) throw '⚠️ 𝙽ú𝚖𝚎𝚛𝚘 𝚍𝚎 𝚜𝚎𝚛𝚒𝚎 𝚒𝚗𝚌𝚘𝚛𝚛𝚎𝚌𝚝𝚘';
+
+    let user = global.db.data.users[m.sender];
+    user.registered = false;
+
+    m.reply(`💙 ¡𝚁𝚎𝚐𝚒𝚜𝚝𝚛𝚘 𝚎𝚕𝚒𝚖𝚒𝚗𝚊𝚍𝚘 𝚌𝚘𝚛𝚛𝚎𝚌𝚝𝚊𝚖𝚎𝚗𝚝𝚎!\n𝙴𝚜𝚙𝚎𝚛𝚊𝚖𝚘𝚜 𝚝𝚞 𝚛𝚎𝚐𝚛𝚎𝚜𝚘 𝚙𝚛𝚘𝚗𝚝𝚘.`);
+
+    const imgUrl = "https://i.imgur.com/P3u2et7.jpg";
+    const responseImg = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+    let txt = `
+┌─「 *𝚁𝚎𝚐𝚒𝚜𝚝𝚛𝚘 𝙴𝚕𝚒𝚖𝚒𝚗𝚊𝚍𝚘* 」
+│
+│🔑 *𝙽ú𝚖𝚎𝚛𝚘 𝚍𝚎 𝚂𝚎𝚛𝚒𝚎:* ${args[0]}
+│
+└─「──────────────」
+`.trim();
+
+    await conn.sendFile(m.chat, responseImg.data, "thumbnail.jpg", txt, m);
+    await m.react("😢");
 }
-handler.help = ['unreg <Num Serie>'] 
-handler.tags = ['rg']
 
-handler.command = ['unreg', 'eliminarrg', 'eliminarregistro'] 
-handler.register = true
+handler.help = ['unreg <Número de Serie>'];
+handler.tags = ['rg'];
+handler.command = ['unreg', 'eliminarrg', 'eliminarregistro'];
+handler.register = true;
 
-export default handler
-
+export default handler;
