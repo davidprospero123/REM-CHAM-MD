00692fe3ff72330d6a3f52660916614c13b3b658
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-04-27 04:26:02 -0500

    REM-BOT

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
     if (conn.user.jid == conn.user.jid) {
         try {
             execSync('git reset --hard HEAD'); 
-            execSync('git clean -fd'); s
+            execSync('git clean -fd'); 
             let stdout = execSync('git pull origin main');
             conn.reply(m.chat, stdout.toString(), m);
             conn.reply(m.chat, '𝙿𝚘𝚛 𝚏𝚊𝚟𝚘𝚛 𝚛𝚎𝚒𝚗𝚒𝚌𝚒𝚎 𝚎𝚕 𝚋𝚘𝚝 𝚎𝚗 𝚕𝚊 𝚌𝚘𝚗𝚜𝚘𝚕𝚊 𝚙𝚊𝚛𝚊 𝚝𝚎𝚛𝚖𝚒𝚗𝚊𝚛 𝚝𝚘𝚍𝚘 𝚌𝚘𝚛𝚛𝚎𝚌𝚝𝚊𝚖𝚎𝚗𝚝𝚎 𝚢 𝚚𝚞𝚎 𝚎𝚕 𝚋𝚘𝚝 𝚊𝚗𝚍𝚎 𝚏𝚞𝚗𝚌𝚒𝚘𝚗𝚊𝚕', m);
