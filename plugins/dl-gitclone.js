d89be85b124136ad100dd06f6d922b1228fb3011
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-05-06 00:38:40 -0500

    REM-BOT

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -1,20 +1,22 @@
-
-import fetch from 'node-fetch'
-const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
+import fetch from "node-fetch";
+const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `¿Dónde está el enlace de github?\n\n📌 Ejemplo : ${usedPrefix + command} https://github.com/davidprospero123/Re-cha-guru`
-    if (!regex.test(args[0])) throw '⚠️ link incorrecto'
-    let [_, user, repo] = args[0].match(regex) || []
-    repo = repo.replace(/.git$/, '')
-    let url = `https://api.github.com/repos/${user}/${repo}/zipball`
-    let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+  if (!args[0])
+    throw `¿Dónde está el enlace de github?\n\n📌 Ejemplo : ${usedPrefix + command} https://github.com/davidprospero123/REM-CHAM`;
+  if (!regex.test(args[0])) throw "⚠️ link incorrecto";
+  let [_, user, repo] = args[0].match(regex) || [];
+  repo = repo.replace(/.git$/, "");
+  let url = `https://api.github.com/repos/${user}/${repo}/zipball`;
+  let filename = (await fetch(url, { method: "HEAD" })).headers
+    .get("content-disposition")
+    .match(/attachment; filename=(.*)/)[1];
 
-    m.reply(`✳️ *Espera, enviando repositorio..*`)
-    conn.sendFile(m.chat, url, filename, null, m)
-}
-handler.help = ['gitclone <url>']
-handler.tags = ['downloader']
-handler.command = ['gitclone'] 
-handler.credit = true
+  m.reply(`✳️ *Espera, enviando repositorio..*`);
+  conn.sendFile(m.chat, url, filename, null, m);
+};
+handler.help = ["gitclone <url>"];
+handler.tags = ["downloader"];
+handler.command = ["gitclone"];
+handler.credit = true;
 
-export default handler
+export default handler;
