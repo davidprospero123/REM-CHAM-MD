984c0bd90b5a5969692b7482f7ec490be1334d9b
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-05-09 00:24:24 -0500

    REM-BOT

--- plugins/_expired-gp.js
+++ plugins/_expired-gp.js
@@ -6,7 +6,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, `🔴 bye bye *${this.user.name}* will leave the group \n\nyour rent ended`)
+        await this.reply(m.chat, `🔴 𝙰𝚍𝚒𝚘𝚜 :𝚌 *${this.user.name}* 𝙼𝚎 𝚜𝚊𝚕𝚍𝚛𝚎 𝚍𝚎𝚕 𝚐𝚛𝚞𝚙𝚘 \n\n𝙰𝚌𝚊𝚋𝚘 𝚜𝚞 𝚛𝚎𝚗𝚝𝚊`)
         await this.groupLeave(m.chat)
         chats.expired = null
     }
