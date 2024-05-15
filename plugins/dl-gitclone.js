bec4be60a9b6a6a4b2211e0204a18718fc7a0999
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-05-14 23:38:37 -0500

    REM-BOT

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0])
-    throw `¿Dónde está el enlace de github?\n\n📌 Ejemplo : ${usedPrefix + command} https://github.com/davidprospero123/REM-CHAM`;
+    throw `¿Dónde está el enlace de github?\n\n📌 Ejemplo : ${usedPrefix + command} https://github.com/davidprospero123/REM-CHAM-MD`;
   if (!regex.test(args[0])) throw "⚠️ link incorrecto";
   let [_, user, repo] = args[0].match(regex) || [];
   repo = repo.replace(/.git$/, "");
