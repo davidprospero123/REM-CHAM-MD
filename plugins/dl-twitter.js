4790a09df0cd83bc9f4be1631571db9e61a67724
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-05-22 12:46:47 -0500

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼-𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻

--- plugins/dl-twitter.js
+++ plugins/dl-twitter.js
@@ -1,24 +1,29 @@
+import fg from 'api-dylux';
 
-  import fg from 'api-dylux'
-  let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `📌 Example : \n*${usedPrefix + command}* https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
-            m.react(rwait)    
-            try {
-            let { SD, HD, desc, thumb, audio } = await fg.twitter(args[0])
-            let te = ` 
-  ┌─⊷ *TWITTER DL*
-  ▢ Description: ${desc}
-  └───────────`
-  conn.sendFile(m.chat, HD, 'twitter.mp4', te, m)
-  m.react(done)
-  } catch (e) {
-      m.reply(`✳️ Verifica Bien tu link de Twitter`)
-    } 
+let handler = async (message, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `📌 𝙴𝚓𝚎𝚖𝚙𝚕𝚘: \n*${usedPrefix + command}* https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`;
 
-  }
-  handler.help = ['twitter'].map(v => v + ' <url>')
-  handler.tags = ['downloader']
-  handler.command = /^(twitter|tw)$/i
-  handler.diamond = true
+  message.react(rwait);
 
-  export default handler;
+  try {
+    let { SD, HD, desc, thumb, audio } = await fg.twitter(args[0]);
+    
+    let template = `
+┌─⊷ *_𝚃𝚆𝙸𝚃𝚃𝙴𝚁 𝙳𝙻_*
+├─📜 𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚌𝚒𝚘𝚗: ${desc}
+├─ 𝚁𝙴𝙼-𝙱𝙾𝚃 𝚋𝚢 𝚌𝚞𝚛𝚒
+└────────────────────`;
+
+    conn.sendFile(message.chat, HD, 'twitter.mp4', template, message);
+    message.react(done);
+  } catch (error) {
+    message.reply(`✳️ 𝚅𝚎𝚛𝚒𝚏𝚒𝚌𝚊 𝚋𝚒𝚎𝚗 𝚝𝚞 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝚃𝚠𝚒𝚝𝚝𝚎𝚛`);
+  } 
+}
+
+handler.help = ['twitter'].map(command => command + ' <url>');
+handler.tags = ['downloader'];
+handler.command = /^(twitter|tw)$/i;
+handler.diamond = true;
+
+export default handler;
