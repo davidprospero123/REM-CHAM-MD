8aa394b4adee5e0ea9956366a60b7c1d40851c65
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-05-22 13:13:27 -0500

    𝚁𝙴𝙼𝙱𝙾𝚃-𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻

--- plugins/test.js
+++ plugins/test.js
@@ -1,8 +1,8 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-    let img = "https://avatars.githubusercontent.com/u/106463398?v=4"
-let info = `*BOT ACTIVADO*`
+    let img = "https://avatars.githubusercontent.com/u/133716066?s=400&u=0c1975543bb02a959e1787e34886329ee7f8949f&v=4"
+let info = `*_𝙼𝙴 𝙼𝙸𝚁𝙰𝚂_*`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
       isForwarded: true, externalAdReply: { title: author, body: botname, sourceUrl: fgyt, thumbnail: await conn.getFile(img) }}})
 }
