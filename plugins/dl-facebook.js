88a12f1fc2fbcf81ede6b4aabe4f52aea75d3270
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-21 08:21:10 -0300

    `𝐑 𝐄 𝐌 - 𝐁 𝐎 𝐓 - 𝐁 𝐘 - 𝐂 𝐔 𝐑 𝐈`

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -1,29 +1,25 @@
 import fg from 'api-dylux';
 
 const handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) {
-    throw `✳️ Por favor envíe el enlace de un vídeo de Facebook\n\n📌 EJEMPLO :\n*${usedPrefix + command}* https://www.facebook.com/Ankursajiyaan/videos/981948876160874/?mibextid=rS40aB7S9Ucbxw6v`;
-  }
+  if (!args[0]) conn.reply(m.chat, `*𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚞𝚗 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔 :𝟹*\n\n*_𝚎𝚓𝚎𝚖𝚙𝚕𝚘_*\n*${usedPrefix + command}* https://www.facebook.com/Ankursajiyaan/videos/981948876160874/?mibextid=rS40aB7S9Ucbxw6v`, m)
 
   const urlRegex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
-  if (!urlRegex.test(args[0])) {
-    throw '⚠️ PLEASE GIVE A VALID URL.';
-  }
+  if (!urlRegex.test(args[0])) conn.reply(m.chat,'𝙿𝚘𝚛𝚏𝚊𝚟𝚘𝚛 𝚛𝚎𝚟𝚒𝚜𝚊 𝚀𝚞𝚎 𝙴𝚕 𝙻𝚒𝚗𝚔 𝚂𝚎𝚊 𝙳𝚎 𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔 :𝟹', m)
 
   m.react(rwait);
 
   try {
     const result = await fg.fbdl(args[0]);
     const tex = `
-⊱ ─── {* REM FBDL*} ─── ⊰
-↳ *Titulo del video:* ${result.title}
+⊱ ─── {* 𝚁𝙴𝙼 - 𝙵𝙱*} ─── ⊰
+↳ *𝚃𝚒𝚝𝚞𝚕𝚘:* ${result.title}
 ⊱ ────── {⋆♬⋆} ────── ⊰`;
 
     const response = await fetch(result.videoUrl);
     const arrayBuffer = await response.arrayBuffer();
     const videoBuffer = Buffer.from(arrayBuffer);
 
-    conn.sendFile(m.chat, videoBuffer, 'fb.mp4', tex, m);
+    conn.sendFile(m.chat, videoBuffer, 'fb.mp4', tex, m, null, rcanal);
     m.react(done);
   } catch (error) {
     console.log(error);
