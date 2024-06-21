8964ee1d3078da0f5edbd1ca6e9d44e92aa5bf82
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-21 09:07:46 -0300

    `𝐑 𝐄 𝐌 - 𝐁 𝐎 𝐓 - 𝐁 𝐘 - 𝐂 𝐔 𝐑 𝐈`

--- plugins/main-Curi.js
+++ plugins/main-Curi.js
@@ -1,6 +1,6 @@
 let handler = async m => m.reply(`
 
-≡  *𝐂𝐔𝐑𝐈  ┃ ᴮᴼᵀ*  ɢʀᴜᴘᴏꜱ ᴀʟ ᴍᴀɴᴅᴏ ᴅᴇ ᴍɪ ᴅᴇ ᴄʀᴇᴀᴅᴏʀ ᴄᴜʀɪ
+≡  *𝗥𝗘𝗠  ┃ ᴮᴼᵀ*  ɢʀᴜᴘᴏꜱ ᴀʟ ᴍᴀɴᴅᴏ ᴅᴇ ᴍɪ ᴅᴇ ᴄʀᴇᴀᴅᴏʀ ᴄᴜʀɪ
 ─────────────
 ▢ ˜”*°•ᴏᴛᴀᴋᴜꜱ ʏ ɢᴀᴍᴇʀꜱ•°*”˜
 https://chat.whatsapp.com/BXf0v0ReIUUHpxVZAK7Xa5
@@ -8,18 +8,19 @@ https://chat.whatsapp.com/BXf0v0ReIUUHpxVZAK7Xa5
 ▢ 𝙊𝙏𝘼𝙆𝙐𝙎 𝙔 𝙂𝘼𝙈𝙀𝙍𝙎 - 𝐂𝐨𝐦𝐮𝐧𝐢𝐝𝐚𝐝
 https://chat.whatsapp.com/BSJ3I4ftuhUIcMV5sJhUGz
 
-▢ ᕼᗴᑎ丅ᗩᎥ - 𝐂𝐨𝐧𝐭𝐞𝐧𝐝𝐢𝐝𝐨 +𝟏𝟖
-https://chat.whatsapp.com/LnQ2JUnmCeM4wi5rCD75Dx
-
 ─────────────
 ≡ 𝙽𝚘 𝚕𝚎 𝚜𝚊𝚋𝚎𝚜? 𝙴𝚗𝚝𝚛𝚊
 
 ▢ 𝐁𝐢𝐞𝐧𝐯𝐞𝐧𝐢𝐝𝐨𝐬 𝐬𝐞𝐚𝐧 
 https://chat.whatsapp.com/BXf0v0ReIUUHpxVZAK7Xa5
 ─────────────
+≡ 𝚂𝚒𝚐𝚞𝚎 𝚎𝚕 𝙲𝚊𝚗𝚊𝚕 :3
+
+• https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+─────────────
 
 ▢ *YouTube*
-• https://www.youtube.com/@BoruteandoBv
+• https://www.youtube.com/@holabb123
 
 
 `.trim())
@@ -28,3 +29,7 @@ handler.tags = ['main']
 handler.command = ['gruposbot', 'groucuri', 'curi', 'ggp', 'gpcuri'] 
 
 export default handler
+
+/* LINK RESTABLECIDO 
+▢ ᕼᗴᑎ丅ᗩᎥ - 𝐂𝐨𝐧𝐭𝐞𝐧𝐝𝐢𝐝𝐨 +𝟏𝟖
+https://chat.whatsapp.com/LnQ2JUnmCeM4wi5rCD75Dx*/
