4a4bd0addd858f189b733644a79fbeecd478ba0f
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-20 22:14:58 -0300

    𝐑 𝐄 𝐌 - 𝐁 𝐎 𝐓  - 𝐁 𝐘  - 𝐂 𝐔 𝐑 𝐈

--- plugins/Wall-Messi.js
+++ plugins/Wall-Messi.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
+
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/Messi.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m)} 
+conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m, null, rcanal)} 
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['messi']
 handler.tags = ['img']
