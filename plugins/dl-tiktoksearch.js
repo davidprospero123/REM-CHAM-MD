4bb9e02a755bd643add33444576c14ab8257b312
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-21 20:58:11 -0300

    `𝐑 𝐄 𝐌 - 𝐁 𝐎 𝐓 - 𝐁 𝐘 - 𝐂 𝐔 𝐑 𝐈`

--- plugins/dl-tiktoksearch.js
+++ plugins/dl-tiktoksearch.js
@@ -2,41 +2,29 @@ import axios from "axios";
 
 let handler = async (m, { conn, usedPrefix, text }) => {
   if (!text)
-    return conn.reply(
-      m.chat,
-      "*🚩 𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚕𝚘 𝚚𝚞𝚎 𝚍𝚎𝚜𝚎𝚊𝚜 𝚋𝚞𝚜𝚌𝚊𝚛 𝚎𝚗 𝚃𝚒𝚔𝚃𝚘𝚔.*",
-      m,
-    );
+    return conn.reply(m.chat, "*𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚕𝚘 𝚚𝚞𝚎 𝚍𝚎𝚜𝚎𝚊𝚜 𝚋𝚞𝚜𝚌𝚊𝚛 𝚎𝚗 𝚃𝚒𝚔𝚃𝚘𝚔.*", m);
   await m.react("💙");
   try {
     let response = await axios.get(`https://delirius-api-oficial.vercel.app/api/tiktoksearch?query=${encodeURIComponent(text)}`);
     let results = response.data.meta;
-    if (!results.length)
-      return conn
-        .reply(
-          m.chat,
-          "No se encontraron resultados, intenta con un nombre más corto.",
-          m,
-        )
-        .then((_) => m.react("✖️"));
-    let txt = `*ＴｉｋＴｏｋ－Ｓｅａｒｃｈ ⇄ Ⅰ<    ⅠⅠ    >Ⅰ   ↻*\n\n`;
+    if (!results.length) return conn.reply(m.chat, "𝙽𝚘 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚎 𝚛𝚎𝚜𝚞𝚕𝚝𝚊𝚍𝚘𝚜, 𝚒𝚗𝚝𝚎𝚗𝚝𝚊 𝚋𝚞𝚜𝚌𝚊𝚗𝚍𝚘 𝚘𝚝𝚛𝚊 𝚌𝚘𝚜𝚊 :𝟹", m).then((_) => m.react("✖️"));
+    
+    let txt = `*ＴｉｋＴｏｋ－Ｓｅａｒｃｈ \n ⇄ Ⅰ<    ⅠⅠ    >Ⅰ   ↻*\n\n`;
     for (let i = 0; i < (30 <= results.length ? 30 : results.length); i++) {
       let video = results[i];
       txt += `\n`;
-      txt += `	❧  *ᴛɪᴛᴜʟᴏ* : ${video.title}\n`;
-      txt += `	❧  *ᴅᴜʀᴀᴄɪÓɴ* : ${video.duration} segundos\n`;
-      txt += `	❧  *ᴜʀʟ* : ${video.url}\n`;
-      txt += `	❧  *ᴀᴜᴛᴏʀ* : ${video.author.username || "×"}\n`;
-      txt += `	❧  *ᴠɪᴇᴡs* : ${video.play}\n`;
-      txt += `	❧  *ᴄᴏʀᴀᴢᴏɴᴇꜱ* : ${video.like}\n\n`;
+      txt += `	❧  *𝚃𝚒𝚝𝚞𝚕𝚘* : ${video.title}\n`;
+      txt += `	❧  *𝙳𝚞𝚛𝚊𝚌𝚒𝚘𝚗* : ${video.duration} segundos\n`;
+      txt += `	❧  *𝚄𝚛𝚕* : ${video.url}\n`;
+      txt += `	❧  *𝙰𝚞𝚝𝚘𝚛* : ${video.author.username || "×"}\n`;
+      txt += `	❧  *𝚅𝚒𝚜𝚒𝚝𝚊𝚜* : ${video.play}\n`;
+      txt += `	❧  *𝙲𝚘𝚛𝚊𝚣𝚘𝚗𝚎𝚜* : ${video.like}\n\n`;
     }
     const url = "https://i.imgur.com/BO4TfMR.png"; 
     const responseImg = await axios.get(url, { responseType: 'arraybuffer' });
-    await conn.sendFile(m.chat, responseImg.data, "thumbnail.png", txt, m); 
+    await conn.sendFile(m.chat, responseImg.data, "thumbnail.png", txt, m, null, rcanal); 
     await m.react("✅");
-  } catch (e) {
-    console.error(e);
-    conn.reply(m.chat, "Ocurrió un error al buscar en TikTok.", m);
+  } catch {
     m.react("❌");
   }
 };
