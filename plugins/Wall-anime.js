7710707d5fe56df734d9d82ce2476894ecceeb9b
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-21 08:09:47 -0300

    `𝐑 𝐄 𝐌 - 𝐁 𝐎 𝐓 - 𝐁 𝐘 - 𝐂 𝐔 𝐑 𝐈`

--- plugins/Wall-anime.js
+++ plugins/Wall-anime.js
@@ -2,7 +2,7 @@ import axios from "axios"
 let handler = async (m, {command, conn, usedPrefix}) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/anime-${command}.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]   
-conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m)
+conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m, null, rcanal)
 //papu si vas usar el api dame creditos :V   
 }
 handler.command = handler.help = ['alisa', 'aihoshino', 'rem', 'akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura']
