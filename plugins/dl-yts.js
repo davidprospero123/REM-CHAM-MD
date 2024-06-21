3231973e5f64a1f8622598f762c4f60fb3df4204
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-21 08:33:18 -0300

    `𝐑 𝐄 𝐌 - 𝐁 𝐎 𝐓 - 𝐁 𝐘 - 𝐂 𝐔 𝐑 𝐈`

--- plugins/dl-yts.js
+++ plugins/dl-yts.js
@@ -1,33 +1,21 @@
 import yts from "yt-search";
 
 let handler = async (m, { conn, usedPrefix, text }) => {
-  if (!text)
-    return conn.reply(
-      m.chat,
-      "*🚩 Ingresa lo que deseas buscar en YouTube.*",
-      m,
-    );
+  if (!text) return conn.reply(m.chat,"*𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚎𝚕 𝚝𝚎𝚡𝚝𝚘 𝚍𝚎 𝚕𝚘 𝚚𝚞𝚎 𝚚𝚞𝚒𝚎𝚛𝚎𝚜 𝚋𝚞𝚜𝚌𝚊𝚛 𝚎𝚗 𝚢𝚘𝚞𝚝𝚞𝚋𝚎 :𝟹*", m, );
   await m.react("💙");
   let results = await yts(text);
   let res = results.all.map((v) => v).filter((v) => v.type == "video");
-  if (!res.length)
-    return conn
-      .reply(
-        m.chat,
-        "No se encontraron resultados, intente con un nombre más Corto.",
-        m,
-      )
-      .then((_) => m.react("✖️"));
+  if (!res.length) return conn.reply(m.chat,"𝚂𝚒𝚗 𝚁𝚎𝚜𝚞𝚕𝚝𝚊𝚍𝚘𝚜, 𝙸𝚗𝚝𝚎𝚗𝚝𝚊 𝙱𝚞𝚜𝚌𝚊𝚗𝚍𝚘 𝙾𝚝𝚛𝚊 𝚌𝚘𝚜𝚊",m,)
   let txt = `*ＹｏｕＴｕｂｅ-Ｓｅａｒｃｈ ⇄ Ⅰ<    ⅠⅠ    >Ⅰ   ↻*`;
   for (let i = 0; i < (30 <= res.length ? 30 : res.length); i++) {
     txt += `\n\n`;
-    txt += `	❧  *ᴛɪᴛᴜʟᴏ* : ${res[i].title}\n`;
-    txt += `	❧  *ᴅᴜʀᴀᴄɪÓɴ* : ${res[i].timestamp || "×"}\n`;
-    txt += `	❧  *ᴘᴜʙʟɪᴄᴀᴅᴏ* : ${res[i].ago}\n`;
-    txt += `	❧  *ᴀᴜᴛᴏʀ* : ${res[i].author.name || "×"}\n`;
-    txt += `	❧  *ᴜʀʟ* : ${"https://youtu.be/" + res[i].videoId}\n`;
+    txt += `	❧  *𝚃𝚒𝚝𝚞𝚕𝚘* : ${res[i].title}\n`;
+    txt += `	❧  *𝙳𝚞𝚛𝚊𝚌𝚒𝚘𝚗* : ${res[i].timestamp || "×"}\n`;
+    txt += `	❧  *𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚍𝚘* : ${res[i].ago}\n`;
+    txt += `	❧  *𝙰𝚞𝚝𝚘𝚛* : ${res[i].author.name || "×"}\n`;
+    txt += `	❧  *𝚄𝚛𝚕* : ${"https://youtu.be/" + res[i].videoId}\n`;
   }
-  await conn.sendFile(m.chat, res[0].image, "", txt, m);
+  await conn.sendFile(m.chat, res[0].image, "", txt, m, null, rcanal);
   await m.react("✅");
 };
 handler.help = ["ytsearch"];
