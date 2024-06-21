a50827b37ec37c7ca99c1c01e0bf185d2c086e0f
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-20 22:06:11 -0300

    𝐑 𝐄 𝐌 - 𝐁 𝐎 𝐓  - 𝐁 𝐘  - 𝐂 𝐔 𝐑 𝐈

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,4 +1,3 @@
-
 import fetch from 'node-fetch'
 
 let handler = m => m
@@ -7,14 +6,16 @@ handler.all = async function (m) {
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
 	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
 	
-	//reply link wa
+	//Reenvio con link del Grupo
    global.rpl = { contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'VIDEO', description: 'support group', title: packname, body: 'support group', thumbnailUrl: pp, sourceUrl: dygp }}} 
 	
-	//reply link PayPal
+	//Reenvio con link de Paypal
     global.rpyp = { contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'VIDEO', description: 'Donate', title: 'YOUTUBE', body: 'Keep bot alive', thumbnailUrl: pp, sourceUrl: fgyt }}}
 	
-	//reply link yt
+	//Reenvio con link de youtube
     global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: fgyt, mediaType: 'VIDEO', description: 'Suscribete : ' + fgyt, title: 'YouTube', body: 'learn to create your own bots', thumbnailUrl: pp, sourceUrl: fgyt }}}
 
+	//Reenvio con link del Canal :3
+ global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: id_canal, serverMessageId: 100, newsletterName: name_canal, }}},	
 } 
 export default handler
