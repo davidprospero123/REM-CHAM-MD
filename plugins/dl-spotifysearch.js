8570b2007cbcbe97e09aed1ef3757fc3530bb50e
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-21 21:00:39 -0300

    `𝐑 𝐄 𝐌 - 𝐁 𝐎 𝐓 - 𝐁 𝐘 - 𝐂 𝐔 𝐑 𝐈`

--- plugins/dl-spotifysearch.js
+++ plugins/dl-spotifysearch.js
@@ -1,40 +1,28 @@
 import fetch from "node-fetch";
 
 let handler = async (m, { conn, usedPrefix, text }) => {
-  if (!text)
-    return conn.reply(
-      m.chat,
-      "*💚 𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚕𝚘 𝚚𝚞𝚎 𝚍𝚎𝚜𝚎𝚊𝚜 𝚋𝚞𝚜𝚌𝚊𝚛 𝚎𝚗 𝚂𝚙𝚘𝚝𝚒𝚏𝚢.*",
-      m,
-    );
+  if (!text) return conn.reply(m.chat,"*💚 𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚕𝚘 𝚚𝚞𝚎 𝚍𝚎𝚜𝚎𝚊𝚜 𝚋𝚞𝚜𝚌𝚊𝚛 𝚎𝚗 𝚂𝚙𝚘𝚝𝚒𝚏𝚢.*", m);
   await m.react("💚");
   let results;
   try {
     results = await fetch(`https://delirius-api-oficial.vercel.app/api/spotify?q=${encodeURIComponent(text)}`).then(res => res.json());
-  } catch (error) {
-    console.error(error);
-    return conn.reply(m.chat, "Ocurrió un error al buscar en Spotify.", m);
+  } catch {
+m.react("❌")
   }
   
   if (!results || !results.data || results.data.length === 0)
-    return conn
-      .reply(
-        m.chat,
-        "𝙽𝚘 𝚜𝚎 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚊𝚛𝚘𝚗 𝚛𝚎𝚜𝚞𝚕𝚝𝚊𝚍𝚘𝚜, 𝚒𝚗𝚝𝚎𝚗𝚝𝚊 𝚌𝚘𝚗 𝚘𝚝𝚛𝚘 𝚝é𝚛𝚖𝚒𝚗𝚘 𝚍𝚎 𝚋ú𝚜𝚚𝚞𝚎𝚍𝚊.",
-        m,
-      )
-      .then((_) => m.react("❌"));
+    return conn.reply(m.chat,"𝙽𝚘 𝚜𝚎 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚊𝚛𝚘𝚗 𝚛𝚎𝚜𝚞𝚕𝚝𝚊𝚍𝚘𝚜, 𝚒𝚗𝚝𝚎𝚗𝚝𝚊 𝚌𝚘𝚗 𝚘𝚝𝚛𝚘 𝚝é𝚛𝚖𝚒𝚗𝚘 𝚍𝚎 𝚋ú𝚜𝚚𝚞𝚎𝚍𝚊.", m).then((_) => m.react("❌"));
 
-  let txt = `*Ｓｐｏｔｉｆｙ-Ｓｅａｒｃｈ ⇄ Ⅰ<    ⅠⅠ    >Ⅰ   ↻*`;
+  let txt = `*Ｓｐｏｔｉｆｙ-Ｓｅａｒｃｈ \n ⇄ Ⅰ<    ⅠⅠ    >Ⅰ   ↻*`;
   for (let i = 0; i < (results.data.length >= 10 ? 10 : results.data.length); i++) {
     const track = results.data[i];
     txt += `\n\n`;
-    txt += `	❧  *ᴛɪᴛʟᴇ* : ${track.title}\n`;
-    txt += `	❧  *ᴀʀᴛɪsᴛ* : ${track.artist}\n`;
-    txt += `	❧  *ᴅᴜʀᴀᴄɪÓɴ* : ${track.duration}\n`;
-    txt += `	❧  *ᴘᴏᴘᴜʟᴀʀɪᴅᴀᴅ* : ${track.popularity}\n`;
-    txt += `	❧  *ᴘᴜʙʟɪᴄᴀᴅᴏ* : ${track.publish}\n`;
-    txt += `	❧  *ʟɪɴᴋ* : ${track.url}\n`;
+    txt += `	❧  *𝚃𝚒𝚝𝚞𝚕𝚘* : ${track.title}\n`;
+    txt += `	❧  *𝙰𝚛𝚝𝚒𝚜𝚝𝚊* : ${track.artist}\n`;
+    txt += `	❧  *𝙳𝚞𝚛𝚊𝚌𝚒𝚘𝚗* : ${track.duration}\n`;
+    txt += `	❧  *𝙿𝚘𝚙𝚞𝚕𝚊𝚛𝚒𝚍𝚊𝚍* : ${track.popularity}\n`;
+    txt += `	❧  *𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚍𝚘* : ${track.publish}\n`;
+    txt += `	❧  *𝙻𝚒𝚗𝚔* : ${track.url}\n`;
   }
   await conn.reply(m.chat, txt, m);
   await m.react("✅");
