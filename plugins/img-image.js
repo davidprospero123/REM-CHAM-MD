c6dedf395bc665793e40e115b3b19713dda3ba59
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-06-22 22:51:24 -0500

    𝚁𝙴𝙼-𝙱𝙾𝚃 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙰𝙻𝙴𝚇

--- plugins/img-image.js
+++ plugins/img-image.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `*🚩 Ingresa que imagen deseas buscar en Google.*`, m,)
 await m.react('🕓')
 let res = await googleImage(text)
-await conn.sendFile(m.chat, res.getRandom(), 'out.png', `*––『ɢᴏᴏɢʟᴇ-ɪᴍᴀɢᴇɴᴇꜱ』––*\n\n*Resultado de ∙* ${text}\n\n𝐑𝐄𝐌 - 𝐁𝐎𝐓 - 𝐁𝐘 - 𝐂𝐔𝐑𝐈`.-trim(),)
+await conn.sendFile(m.chat, res.getRandom(), 'out.png', `*––『ɢᴏᴏɢʟᴇ-ɪᴍᴀɢᴇɴᴇꜱ』––*\n\n*Resultado de ∙* ${text}\n\n𝐑𝐄𝐌 - 𝐁𝐎𝐓 - 𝐁𝐘 - 𝐂𝐔𝐑𝐈`.trim(),)
 await m.react('✅')
 }
 handler.help = ['imagen <texto>']
