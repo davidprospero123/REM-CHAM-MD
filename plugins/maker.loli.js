f5c1d2ec35971aff510fcc73d893bf6e75fba911
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-24 07:09:50 -0300

    𝚁𝙴𝙼-𝙱𝙾𝚃 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙰𝙻𝙴𝚇

--- plugins/maker.loli.js
+++ plugins/maker.loli.js
@@ -2,9 +2,11 @@ let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/lolice', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    }), 'error.png', '*🚔🚨 ᴅᴇɴᴜɴᴄɪᴀᴅᴏ ᴘᴀᴘᴜ 🚨🚔*', m)
+    }), 'error.png', '*🚔🚨 ᴅᴇɴᴜɴᴄɪᴀᴅᴏ ᴘᴀᴘᴜ 🚨🚔*', m, null, rcanal)
     }
+
     handler.help = ['lolicon']
     handler.tags = ['maker']
     handler.command = /^(lolicon)$/i
+
     export default handler
