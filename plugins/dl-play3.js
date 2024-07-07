eea86c89090fb1746c9fb679dad8e6dae78f0fe3
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-07-07 12:15:09 -0500

    𝚁𝙴𝙼-𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙹𝚃𝚡𝚜

--- plugins/dl-play3.js
+++ plugins/dl-play3.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 
 
-const imgUrl = 'https://i.imgur.com/JdMcnNQ.jpeg';
+const imgUrl = 'https://telegra.ph/file/a83d8f5535e6b744986b4.png';
 
 let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     let lister = ["mp3", "yta", "audio", "ytv", "video", "vídeo", "mp4", "mp3doc", "ytadoc", "audiodoc", "mp4doc", "ytvdoc", "videodoc", "vídeodoc"];
