0a12b76f8052e8fe015deb503889617f1a1cd843
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-07-16 11:25:59 -0500

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻-𝙹𝚃𝚇𝚂

--- plugins/Wall-anime.js
+++ plugins/Wall-anime.js
@@ -5,6 +5,6 @@ let haha = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m, null, rcanal)
 //papu si vas usar el api dame creditos :V   
 }
-handler.command = handler.help = ['alisa', 'aihoshino', 'remcham', 'akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura']
+handler.command = handler.help = ['alisa', 'aihoshino', 'remcham', 'akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumitokisaki', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura']
 handler.tags = ['anime']
 export default handler
