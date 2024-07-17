d5b6de703a724bd644d4ae20bf92c3087a9cb14a
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-07-17 17:24:58 -0500

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙹𝚃𝚇𝚂

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,38 +1,65 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
 
-import fetch from 'node-fetch'
-import { mediafiredl } from '@bochilteam/scraper'
+const handler = async (m, { conn, args }) => {
+    if (!args[0]) throw `♡ 𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚎𝚕 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝙼𝚎𝚍𝚒𝚊𝙵𝚒𝚛𝚎 𝚊𝚕 𝚕𝚊𝚍𝚘 𝚍𝚎𝚕 𝚌𝚘𝚖𝚊𝚗𝚍𝚘`;
+    if (!args[0].match(/mediafire/gi)) throw `♡ 𝙴𝚗𝚕𝚊𝚌𝚎 𝚗𝚘 𝚟𝚊𝚕𝚒𝚍𝚘`;
 
-let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) => {
-  var limit
-     if((isOwner || isPrems)) limit = 1200
-     else limit = 100
-   if (!args[0]) throw `✳️ 𝙸𝚗𝚐𝚛𝚎𝚜𝚎 𝚎𝚕 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝚖𝚎𝚍𝚒𝚊𝚏𝚒𝚛𝚎 𝚊𝚕 𝚕𝚊𝚍𝚘 𝚍𝚎𝚕 𝚌𝚘𝚖𝚊𝚗𝚍𝚘`
-    if (!args[0].match(/mediafire/gi)) throw `❎ 𝙻𝚒𝚗𝚔 𝚒𝚗𝚌𝚘𝚛𝚛𝚎𝚌𝚝𝚘`
-    m.react(rwait)
-    let full = /f$/i.test(command)
-    let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-    let ss = await (await fetch(`https://image.thum.io/get/fullpage/${u}`)).buffer()
-    let res = await mediafiredl(args[0])
-    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let isLimit = (isPrems || isOwner ? limit : limit) * 1012 < filesize
+    m.react('💙'); 
+
+    const getDownloadDetails = async (url) => {
+        try {
+            const { data } = await axios.get(url);
+            const $ = cheerio.load(data);
+
+            const downloadLink = $('a[aria-label="Download file"]').attr('href');
+            const fileName = $('.filename').text().trim();
+
+            if (downloadLink && fileName) {
+                return { downloadLink, fileName };
+            } else {
+                throw new Error('𝙽𝚘 𝚜𝚎 𝚙𝚞𝚍𝚘 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚊𝚛 𝚎𝚕 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝚍𝚎𝚜𝚌𝚊𝚛𝚐𝚊 𝚘 𝚎𝚕 𝚗𝚘𝚖𝚋𝚛𝚎 𝚍𝚎𝚕 𝚊𝚛𝚌𝚑𝚒𝚟𝚘');
+            }
+        } catch (error) {
+            console.error('Error durante el proceso:', error);
+            return null;
+        }
+    };
+
+    let url = args[0];
+    let details = await getDownloadDetails(url);
+
+    if (!details) throw `𝙴𝚛𝚛𝚘𝚛 𝚊𝚕 𝚘𝚋𝚝𝚎𝚗𝚎𝚛 𝚎𝚕 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝚍𝚎𝚜𝚌𝚊𝚛𝚐𝚊.`;
+
+    let response = {
+        success: true,
+        creator: "Gabriel Curi",
+        title: details.fileName,
+        downloadLink: details.downloadLink,
+    };
+
     let caption = `
-   ≡ *𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴*
+╭────〈 *MEDIAFIRE* 〉────
+│
+│ ├── >𝙽𝙾𝙼𝙱𝚁𝙴: ${response.title}
+│ ├── >𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰: ${response.downloadLink}
+│ ├── >𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 𝙶𝚊𝚋𝚛𝚒𝚎𝚕
+╰─────────────────────
+    `.trim();
 
-▢ *𝙽𝙾𝙼𝙱𝚁𝙴:* ${filename}
-▢ *𝙿𝙴𝚂𝙾:* ${filesizeH}
-▢ *𝙴𝚇𝚃𝙴𝙽𝙲𝙸𝙾𝙽:* ${ext}
-▢ *𝙿𝚄𝙱𝙻𝙸𝙲𝙰𝙳𝙾:* ${aploud}
-${isLimit ? `\n▢ 𝙴𝚜𝚝𝚎 𝚊𝚛𝚌𝚑𝚒𝚟𝚘 𝚎𝚡𝚌𝚎𝚍𝚎 𝚕𝚘 𝚕𝚒𝚖𝚒𝚝𝚎𝚜 *+${limit} MB*\n𝙼𝚎𝚓𝚘𝚛𝚊𝚝𝚎 𝚊 𝚙𝚛𝚎𝚖 𝚢 𝚍𝚎𝚜𝚌𝚊𝚛𝚐𝚊𝚛𝚊𝚜 𝚑𝚊𝚜𝚝𝚊 *900 MB*` : ''} 
-`.trim()
-    await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
+    const imageUrl = "https://telegra.ph/file/b09e343ef667887945c0a.jpg";
+    const responseImg = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    await conn.sendFile(m.chat, responseImg.data, "thumbnail.png", caption, m);
 
-    if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-    m.react(done)
-}
-handler.help = ['mediafire <url>']
-handler.tags = ['downloader', 'premium']
-handler.command = ['mediafire', 'mfire'] 
-handler.register = true
-handler.premium = false
+    await conn.sendFile(m.chat, details.downloadLink, response.title, '', m);
 
-export default handler
+    m.react('✔️'); 
+};
+
+handler.help = ['mediafire <url>'];
+handler.tags = ['downloader', 'premium'];
+handler.command = ['mediafire', 'mfire'];
+handler.register = true;
+handler.premium = false;
+
+export default handler;
