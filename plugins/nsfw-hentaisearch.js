f6895eb7ab83224beedf25e64a9ee2a5fd9d5422
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-07-28 21:07:03 -0300

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙹𝚃𝚇𝚂

--- plugins/nsfw-hentaisearch.js
+++ plugins/nsfw-hentaisearch.js
@@ -2,7 +2,8 @@ let handler = async (m, { conn, text, usedPrefix, command, args }) => {
     if (!global.db.data.chats[m.chat].nsfw) throw `🚫 𝙽𝚘 𝙴𝚜𝚝𝚊 𝙰𝚌𝚝𝚒𝚟𝚊𝚍𝚘 𝙴𝚕 𝙽𝚂𝙵𝚆 𝙴𝚗 𝙴𝚜𝚝𝚎 𝙶𝚛𝚞𝚙𝚘\n\n 𝙰𝚌𝚝𝚒𝚟𝚊𝚕𝚘 𝚄𝚜𝚊𝚗𝚍𝚘 \n*${usedPrefix}𝙴𝚗𝚊𝚋𝚕𝚎 𝚗𝚜𝚏𝚠`
     let user = global.db.data.users[m.sender].age
     if (user < 17) throw m.reply(`❎ 𝙽𝚎𝚌𝚎𝚜𝚒𝚝𝚊𝚜 𝚃𝚎𝚗𝚎𝚛 +𝟷𝟾 𝙿𝚊𝚛𝚊 𝚄𝚜𝚊𝚛 𝙴𝚜𝚝𝚎 𝙲𝚘𝚖𝚊𝚗𝚍𝚘`)
-    if (!text) return m.reply(`*𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝙴𝚕 𝚃𝚎𝚡𝚝𝚘 𝙳𝚎 𝙰𝚕𝚐𝚞𝚗 𝙷𝚎𝚗𝚝𝚊𝚒 𝚀𝚞𝚎 𝚀𝚞𝚒𝚎𝚛𝚊𝚜 𝙱𝚞𝚜𝚌𝚊𝚛*\n\n*_𝚎𝚓𝚎𝚖𝚙𝚕𝚘_*\n${usedPrefix + command} miku*`)
+    
+    if (!text) return m.reply(`*𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝙴𝚕 𝚃𝚎𝚡𝚝𝚘 𝙳𝚎 𝙰𝚕𝚐𝚞𝚗 𝙷𝚎𝚗𝚝𝚊𝚒 𝚀𝚞𝚎 𝚀𝚞𝚒𝚎𝚛𝚊𝚜 𝙱𝚞𝚜𝚌𝚊𝚛*`)
     try {
     m.reply(global.wait)
     let res = await fetch(`https://api.lolhuman.xyz/api/nhentaisearch?apikey=${lolkeysapi}&query=${text}`)    
@@ -16,5 +17,8 @@ let handler = async (m, { conn, text, usedPrefix, command, args }) => {
     } catch {
     throw `𝙾𝚌𝚞𝚛𝚛𝚒𝚘 𝚄𝚗 𝙴𝚛𝚛𝚘𝚛 𝙸𝚗𝚎𝚜𝚙𝚎𝚛𝚊𝚍𝚘 :(`
     }}
-    handler.command = /^(hentai)$/i
-    export default handler
+handler.help = ['hentai <texto>']
+handler.tags = ['nsfw']
+handler.command = ['hentai', 'hentaisearch']
+
+export default handler
