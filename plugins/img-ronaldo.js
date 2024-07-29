5745b873739a2d57db5e36cbc70ba2e5a4060a47
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-07-28 21:53:22 -0300

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙹𝚃𝚇𝚂

--- plugins/img-ronaldo.js
+++ plugins/img-ronaldo.js
@@ -3,10 +3,13 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let cristiano = (
     await axios.get(`https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/CristianoRonaldo.json`,)).data;
   let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())];
-  conn.sendFile(m.chat, ronaldo, "error.jpg", `*siuuuuuuuuuu*`, m, null, rcanal);
+  conn.sendFile(m.chat, ronaldo, "error.jpg", `*CR7*`, m, null, rcanal);
 };
+
 //conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ["cristianoronaldo", "cr7"];
+
+handler.help = ["cr7"];
 handler.tags = ["img"];
 handler.command = /^(ronaldo|cr7)$/i;
+
 export default handler;
