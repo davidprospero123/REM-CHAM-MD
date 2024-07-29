e11a413b01c3c7dc516c6b8c16aeb83e917f57e4
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-07-28 21:46:19 -0300

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙹𝚃𝚇𝚂

--- plugins/img-animes.js
+++ plugins/img-animes.js
@@ -1,10 +1,16 @@
+/*
+AVISO : si vas a usar la api ( https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/anime-${command}.json ) dejale creditos al curly, digo digo, curi
+*/
+
 import axios from "axios"
+
 let handler = async (m, {command, conn, usedPrefix}) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/anime-${command}.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]   
-conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m, null, rcanal)
-//papu si vas usar el api dame creditos :V   
+conn.sendFile(m.chat, haha, 'error.jpg', `*${command}*`, m, null, rcanal)
 }
+
 handler.command = handler.help = ['alisa', 'aihoshino', 'remcham', 'akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumitokisaki', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura']
 handler.tags = ['anime']
+
 export default handler
