ae012abdffaca79b68ca4b9def08365dfdb54284
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-07-28 21:12:57 -0300

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙹𝚃𝚇𝚂

--- plugins/nsfw-rule34.js
+++ plugins/nsfw-rule34.js
@@ -1,16 +1,18 @@
 import { googleImage, pinterest } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!global.db.data.chats[m.chat].nsfw) return conn.reply(m.chat, `❎ En este grupo no esta permitido el contenido *+18*`, m,).then(_ => m.react('✖️'))
-
-if (!text) return conn.reply(m.chat, `*🚩 Ingresa un texto junto al comando.*`, m,)
+if (!global.db.data.chats[m.chat].nsfw) throw `🚫 𝙽𝚘 𝙴𝚜𝚝𝚊 𝙰𝚌𝚝𝚒𝚟𝚊𝚍𝚘 𝙴𝚕 𝙽𝚂𝙵𝚆 𝙴𝚗 𝙴𝚜𝚝𝚎 𝙶𝚛𝚞𝚙𝚘\n\n 𝙰𝚌𝚝𝚒𝚟𝚊𝚕𝚘 𝚄𝚜𝚊𝚗𝚍𝚘 \n*${usedPrefix}𝙴𝚗𝚊𝚋𝚕𝚎 𝚗𝚜𝚏𝚠`
+let user = global.db.data.users[m.sender].age
+if (user < 17) throw m.reply(`❎ 𝙽𝚎𝚌𝚎𝚜𝚒𝚝𝚊𝚜 𝚃𝚎𝚗𝚎𝚛 +𝟷𝟾 𝙿𝚊𝚛𝚊 𝚄𝚜𝚊𝚛 𝙴𝚜𝚝𝚎 𝙲𝚘𝚖𝚊𝚗𝚍𝚘`)
+  
+if (!text) return conn.reply(m.chat, `*𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝙴𝚕 𝚃𝚎𝚡𝚝𝚘 𝙳𝚎 𝚕𝚘 𝚀𝚞𝚎 𝚀𝚞𝚒𝚎𝚛𝚊𝚜 𝙱𝚞𝚜𝚌𝚊𝚛*`, m,)
 await m.react('🕓')
 const res = await (await googleImage('rule34 ' + text)).getRandom()
-await conn.sendFile(m.chat, res, 'error.jpg', `*––––『 ɴꜱꜰᴡ ʀᴜʟᴇ34 』––––*\n\n*Resultado de ∙* ${text ? text.capitalize() : false}\n\n𝙍𝙚𝙢-𝘾𝙝𝙖𝙢`,)
+await conn.sendFile(m.chat, res, 'error.jpg', `*––––『 ɴꜱꜰᴡ ʀᴜʟᴇ34 』––––*\n\n*Resultado de :* ${text ? text.capitalize() : false}`, m, null, rcanal)
 await m.react('✅')
 }
 handler.help = ['rule34 <texto>']
 handler.tags = ['nsfw', 'img']
 handler.command = ['rule34']
 handler.gold = 5
-handler.group = true 
+
 export default handler
