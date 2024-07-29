b3984a86f7ed8f5cc193412a41f16e56416fa993
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-07-28 21:43:08 -0300

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙹𝚃𝚇𝚂

--- plugins/img-messi.js
+++ plugins/img-messi.js
@@ -5,7 +5,9 @@ let res = (await axios.get(`https://raw.githubusercontent.com/davidprospero123/a
 let url = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m, null, rcanal)} 
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+
 handler.help = ['messi']
 handler.tags = ['img']
 handler.command = /^(messi)$/i
+
 export default handler
