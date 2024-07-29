cd3c7bd2e1cfcb1c4f6a53fef97125bfaeb0d4fe
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-07-29 16:02:38 -0500

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙹𝚃𝚇𝚂

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,36 +1,32 @@
-import { tiktokdl } from '@bochilteam/scraper';
-import fg from 'api-dylux';
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-
- if (!args[0] && m.quoted && m.quoted.text) {
-  args[0] = m.quoted.text;
-}
-if (!args[0] && !m.quoted) throw `Dale el enlace del video Tiktok o cita un enlace de tiktok`;
- if (!args[0].match(/tiktok/gi)) throw `Verifica el link del tik tok`;
-
-
-  let txt = 'Aqui Esta Tu Video';
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) return m.reply('*Ingresa un enlace de TikTok*');
 
   try {
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]);
-    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
+    await m.react('🕓'); 
+    const apiResponse = await fetch(`https://thepapusapi.koyeb.app/api/tiktok/?url=${args[0]}`);
+    const responseData = await apiResponse.json();
+    
+    if (responseData.status) {
+      const { creator, data } = responseData;
+      const { author, desc, cover, result } = data;
+      let txt = '    `TikTok downloader`\n\n';
+      txt += `> *Autor*: _${author}_\n`;
+      txt += `> *Descripción*: ${desc}\n`;
+      txt += `> *Cover*: ${cover}\n`;
 
-    if (!url) throw global.error;
-
-    conn.sendFile(m.chat, url, 'tiktok.mp4', '', m);
-  } catch (err) {
-    try {
-      let p = await fg.tiktok(args[0]);
-      conn.sendFile(m.chat, p.play, 'tiktok.mp4', txt, m);
-    } catch {
-      m.reply('*Ocurrio un error*');
+      await conn.sendMessage(m.chat, { video: { url: result.mp4NoWm[0] }, caption: txt }, { quoted: m });
+      await m.react('✅'); 
+    } else {
+      await m.react('❌'); 
     }
+  } catch {
+    await m.react('❌'); 
   }
-};
+}
 
-handler.help = ['tiktok'].map((v) => v + ' <url>');
-handler.tags = ['downloader'];
-handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i;
-
+handler.help = ['tiktok *<link>*'];
+handler.tags = ['dl'];
+handler.command = ['tiktok', 'tt', 'TikTok', 'TK'];
 export default handler;
