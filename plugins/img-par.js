57a2aee33e2bb64eca2dfa7d25888dc47b5e8b8c
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-07-28 22:02:04 -0300

    𝚁𝙴𝙼𝙲𝙷𝙰𝙼 𝙱𝚈 𝙶𝙰𝙱𝚁𝙸𝙴𝙻 - 𝙹𝚃𝚇𝚂

--- plugins/img-par.js
+++ plugins/img-par.js
@@ -1,14 +1,12 @@
 import parImages from '../lib/parImages.js'; 
 
 let handler = async (m, { conn }) => {
-    try {
-        const { male, female } = parImages.default[Math.floor(Math.random() * parImages.default.length)]; 
+try {
+const { male, female } = parImages.default[Math.floor(Math.random() * parImages.default.length)]; 
         
+await conn.sendFile(m.chat, male, 'male.jpg', '😺 *Ｃｈｉｃｏ*`', m, null, rcanal);
         
-        await conn.sendFile(m.chat, male, 'male.jpg', '😺 *Ｃｈｉｃｏ*\n\n`𝐑𝐄𝐌 -𝐁𝐎𝐓 - 𝐁𝐘 - 𝐂𝐔𝐑𝐈`', m, null, rcanal);
-        
-        
-        await conn.sendFile(m.chat, female, 'female.jpg', '🐢 *Ｃｈｉｃａ*\n\n`𝐑𝐄𝐌 -𝐁𝐎𝐓 - 𝐁𝐘 - 𝐂𝐔𝐑𝐈`', m, null, rcanal);
+await conn.sendFile(m.chat, female, 'female.jpg', '🐢 *Ｃｈｉｃａ*`', m, null, rcanal);
     } catch (error) {
         console.error(error);
     }
