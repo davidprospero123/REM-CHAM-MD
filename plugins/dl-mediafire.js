4dea13c646a1f22780b98d7fbb1d2b9eda9d5f51
GabrielCuri <133716066+davidprospero123@users.noreply.github.com>
2024-09-29 21:18:18 -0500

    𝙋𝘼𝙍𝘾𝙃𝙀 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀-𝘿𝙇

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,65 +1,65 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import path from 'path';
 
-const handler = async (m, { conn, args }) => {
-    if (!args[0]) throw `♡ 𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚎𝚕 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝙼𝚎𝚍𝚒𝚊𝙵𝚒𝚛𝚎 𝚊𝚕 𝚕𝚊𝚍𝚘 𝚍𝚎𝚕 𝚌𝚘𝚖𝚊𝚗𝚍𝚘`;
-    if (!args[0].match(/mediafire/gi)) throw `♡ 𝙴𝚗𝚕𝚊𝚌𝚎 𝚗𝚘 𝚟𝚊𝚕𝚒𝚍𝚘`;
-
-    m.react('💙'); 
-
-    const getDownloadDetails = async (url) => {
-        try {
-            const { data } = await axios.get(url);
-            const $ = cheerio.load(data);
-
-            const downloadLink = $('a[aria-label="Download file"]').attr('href');
-            const fileName = $('.filename').text().trim();
-
-            if (downloadLink && fileName) {
-                return { downloadLink, fileName };
-            } else {
-                throw new Error('𝙽𝚘 𝚜𝚎 𝚙𝚞𝚍𝚘 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚊𝚛 𝚎𝚕 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝚍𝚎𝚜𝚌𝚊𝚛𝚐𝚊 𝚘 𝚎𝚕 𝚗𝚘𝚖𝚋𝚛𝚎 𝚍𝚎𝚕 𝚊𝚛𝚌𝚑𝚒𝚟𝚘');
-            }
-        } catch (error) {
-            console.error('Error durante el proceso:', error);
-            return null;
-        }
-    };
-
-    let url = args[0];
-    let details = await getDownloadDetails(url);
-
-    if (!details) throw `𝙴𝚛𝚛𝚘𝚛 𝚊𝚕 𝚘𝚋𝚝𝚎𝚗𝚎𝚛 𝚎𝚕 𝚎𝚗𝚕𝚊𝚌𝚎 𝚍𝚎 𝚍𝚎𝚜𝚌𝚊𝚛𝚐𝚊.`;
-
-    let response = {
-        success: true,
-        creator: "Gabriel Curi",
-        title: details.fileName,
-        downloadLink: details.downloadLink,
-    };
-
-    let caption = `
-╭────〈 *MEDIAFIRE* 〉────
-│
-│ ├── >𝙽𝙾𝙼𝙱𝚁𝙴: ${response.title}
-│ ├── >𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰: ${response.downloadLink}
-│ ├── >𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 𝙶𝚊𝚋𝚛𝚒𝚎𝚕
-╰─────────────────────
-    `.trim();
-
-    const imageUrl = "https://telegra.ph/file/b09e343ef667887945c0a.jpg";
+let handler = async (message, { conn, args, usedPrefix, text, command }) => {
+  try {
+    if (!text) {
+      return message.reply(`${usedPrefix + command} https://www.mediafire.com/file/example`);
+    }
+    const response = await axios.get(`https://remcham-bot.vercel.app/api/mediafire?url=${text}`);
+    const { data } = response.data;
+    if (!response.data.success) {
+      return message.reply('Error: No se pudo obtener los detalles del archivo.');
+    }
+    const {
+      url,
+      filename,
+      filesizeH,
+      ext,
+      filetype,
+      uploadDate
+    } = data;
+    const downloadingMessage = `🌩️ *_ꜱᴜ ᴅᴇꜱᴄᴀʀɢᴀ ᴇꜱᴛᴀ ᴄᴏᴍᴇɴᴢᴀɴᴅᴏ...._*`;
+    const imageUrl = 'https://i.pinimg.com/736x/e4/7c/be/e47cbe22aae75ef2f22962cc022d6bac.jpg';
     const responseImg = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    await conn.sendFile(m.chat, responseImg.data, "thumbnail.png", caption, m);
-
-    await conn.sendFile(m.chat, details.downloadLink, response.title, '', m);
-
-    m.react('✔️'); 
+    await conn.sendFile(message.chat, responseImg.data, 'thumbnail.png', downloadingMessage, message);
+    const caption = `*_ＤＥＳＣＡＲＧＡ ＥＸＩＴＯＳＡ..._*\n*_ᴀʀᴄʜɪᴠᴏ_*: ${filename}\n*_ᴛᴀᴍᴀÑᴏ_*: ${filesizeH}\n*_ᴛɪᴘᴏ_*: ${filetype}\n*_ᴇxᴛᴇɴᴄɪᴏɴ_*: ${ext}\n*_ᴘᴜʙʟɪᴄᴀᴅᴏ ᴇʟ_*: ${uploadDate}`;
+    const fileData = await axios({
+      method: 'get',
+      url,
+      responseType: 'arraybuffer'
+    });
+    const fileExtension = path.extname(filename).toLowerCase();
+    const mimeTypes = {
+      '.mp4': 'video/mp4',
+      '.pdf': 'application/pdf',
+      '.zip': 'application/zip',
+      '.rar': 'application/x-rar-compressed',
+      '.7z': 'application/x-7z-compressed',
+      '.jpg': 'image/jpeg',
+      '.jpeg': 'image/jpeg',
+      '.png': 'image/png',
+      '.apk': 'application/vnd.android.package-archive'
+    };
+    let mimetype = mimeTypes[fileExtension] || 'application/octet-stream';
+    await conn.sendFile(message.chat, fileData.data, filename, caption, message, null, { mimetype, asDocument: true });
+  } catch (error) {
+    return message.reply(`Error: ${error.message}`);
+  }
 };
 
-handler.help = ['mediafire <url>'];
-handler.tags = ['downloader', 'premium'];
-handler.command = ['mediafire', 'mfire'];
-handler.register = true;
-handler.premium = false;
+handler.help = ['mediafire'];
+handler.tags = ['downloader'];
+handler.command = /^(mediafire|mf)$/i;
 
 export default handler;
+
+function formatBytes(bytes) {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log (k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
